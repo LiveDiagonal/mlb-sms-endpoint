590f2c9c48e4017eaa095dc91942786454807551
Anthony D'Addeo <amdaddeo@gmail.com>
2017-05-20 14:47:24 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+var games = require('./services/games');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/game', function(request, response) {
+	var callback = function (status, message) {
+		response.json({
+			success: status,
+  			message: message
+  		});
+	}
+
+  	games.todaysGame(request.query.team, callback);	
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
---
+++ services/games.js
@@ -0,0 +1,28 @@
+var request = require('request')
+
+var date = new Date();
+var url = "http://gdx.mlb.com/components/game/mlb/year_" + date.getFullYear() + "/month_" + pad(date.getMonth() + 1) + "/day_" + pad(date.getDate()) + "/master_scoreboard.json"
+
+
+module.exports = {
+	todaysGame(team, callback) {
+		request(url, function (error, response, body) {
+			var games = JSON.parse(body).data.games.game;
+
+			gameFound = false;
+			games.forEach(function (game){
+		        if (game.home_team_name.toLowerCase() == team.toLowerCase() || game.home_code.toLowerCase() == team.toLowerCase() || game.home_name_abbrev.toLowerCase() == team.toLowerCase() ||  game.away_team_name.toLowerCase() == team.toLowerCase() || game.away_code.toLowerCase() == team.toLowerCase() || game.away_name_abbrev.toLowerCase() == team.toLowerCase()) {
+		          gameFound = true;
+		          callback(true, game.game_media.media.title + ", " + game.time + " " + game.time_zone) ;
+		        }
+		    })
+
+		    if (!gameFound)
+		    	callback(false, "Game not found.")
+		});
+	}
+}
+
+function pad(n){
+    return n > 9 ? "" + n: "0" + n;
+}
