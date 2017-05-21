a7208cf20174d78c560128789c9afa2ff66e5287
Anthony D'Addeo <amdaddeo@gmail.com>
2017-05-21 00:25:58 -0500

    Command parsing

--- index.js
+++ index.js
@@ -5,27 +5,42 @@ var games = require('./services/games');
 
 app.set('port', (process.env.PORT || 5001));
 
-app.get('/game', function(request, response) {
+app.get('/execute', function(request, response) {
 	var callback = function (status, message) {
 		response.json({
-			success: status,
-  			message: message
-  		})
+      success: status,
+      message: message
+    })
 	}
 
-  var commandObj = parseCommand(request.query.command)
-  games.todaysGame(commandObj.command, callback);	
+  executeCommand(parseCommand(request.query.command), callback)
 })
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
 
-var parseCommand = function (command) {
-  var matches = command.toLowerCase().match(/(\w+)(.*)/)
+
+var parseCommand = function (command = "") {
+  var matches = command.toLowerCase().match(/(\w+)\s*(.*)/)
   
   return {
-    command: (matches.length > 1) ? matches[1] : null,
-    parameters: (matches.length > 2) ? matches[2] : null,
+    command: (matches && matches.length > 1) ? matches[1] : null,
+    parameters: (matches && matches.length > 2) ? matches[2].split(/\s+/) : null,
+  }
+}
+
+
+var commandFns = {
+  "game": games.todaysGame
+}
+
+var executeCommand = function (commandObj, callback) {
+  commandFn = commandFns[commandObj.command]
+
+  if (commandFn != null) {
+    commandFn(commandObj.parameters, callback)
+  } else {
+    callback(false, "Command " + commandObj.command + " not found.")
   }
 }
--- services/games.js
+++ services/games.js
@@ -5,24 +5,26 @@ var url = "http://gdx.mlb.com/components/game/mlb/year_" + date.getFullYear() + 
 
 
 module.exports = {
-	todaysGame(team = "", callback) {
+	todaysGame(parameters, callback) {
+		var team = parameters[0] || "";
+
 		request(url, function (error, response, body) {
 			var games = JSON.parse(body).data.games.game;
 
 			gameFound = false;
 			games.forEach(function (game){
-		        if (game.home_team_name.toLowerCase() == team.toLowerCase() || game.home_code.toLowerCase() == team.toLowerCase() || game.home_name_abbrev.toLowerCase() == team.toLowerCase() ||  game.away_team_name.toLowerCase() == team.toLowerCase() || game.away_code.toLowerCase() == team.toLowerCase() || game.away_name_abbrev.toLowerCase() == team.toLowerCase()) {
-		          gameFound = true;
-		          callback(true, game.game_media.media.title + ", " + game.time + " " + game.time_zone) ;
-		        }
-		    })
+				if (game.home_team_name.toLowerCase() == team.toLowerCase() || game.home_code.toLowerCase() == team.toLowerCase() || game.home_name_abbrev.toLowerCase() == team.toLowerCase() ||  game.away_team_name.toLowerCase() == team.toLowerCase() || game.away_code.toLowerCase() == team.toLowerCase() || game.away_name_abbrev.toLowerCase() == team.toLowerCase()) {
+					gameFound = true;
+					callback(true, game.game_media.media.title + ", " + game.time + " " + game.time_zone) ;
+				}
+			})
 
-		    if (!gameFound)
-		    	callback(false, "Game not found.")
+			if (!gameFound)
+				callback(false, "Game not found for " + team + ".")
 		});
 	}
 }
 
 function pad(n){
-    return n > 9 ? "" + n: "0" + n;
+	return n > 9 ? "" + n: "0" + n;
 }
