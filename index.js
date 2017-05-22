8a577404b04eef437a390d192ddbd2560bd97cc7
Anthony D'Addeo <amdaddeo@gmail.com>
2017-05-22 18:51:37 -0500

    Refactor using Promises

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
 var express = require('express')
 var app = express()
 
-var games = require('./services/games')
+var processCommand = require('./lib/process_command')
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/execute', function(request, response) {
-	var callback = function (status, message) {
-		response.json({
-      message: message
+  processCommand(request.query.command)
+    .then(function (message) {
+      response.json({ message: message })
     })
-	}  
-
-  executeCommand(parseCommand(request.query.command), callback)
-})
+    .catch(function (message) {
+      response.json({ message: message })
+    })
+ })
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
-
-var parseCommand = function (command) {
-  var matches = command.toLowerCase().match(/(\w+)\s*(.*)/)
-  
-  return {
-    command: (matches && matches.length > 1) ? matches[1] : null,
-    parameters: (matches && matches.length > 2) ? matches[2].split(/\s+/) : null,
-  }
-}
-
-
-var commandFns = {
-  "game": games.todaysGame,
-  "score": games.score,
-  "diff": games.scoreDifferential
-}
-
-var executeCommand = function (commandObj, callback) {
-  commandFn = commandFns[commandObj.command]
-
-  if (commandFn !== null) {
-    commandFn(commandObj.parameters, callback)
-  } else {
-    callback(false, "Command " + commandObj.command + " not found.")
-  }
-}
